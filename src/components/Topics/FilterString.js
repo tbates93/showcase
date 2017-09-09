e849e783b591b7e5b168b7c04f0217ad32e1e1c2
Travis Bates <travbates93@gmail.com>
2017-09-08 21:51:54 -0600

    Filter String done

--- src/components/Topics/FilterString.js
+++ src/components/Topics/FilterString.js
@@ -14,12 +14,17 @@ export default class FilterStrings extends Component {
         this.setState({userInput: str})
     }
 
-    search(){
-        var returnArr = this.state.UnfilteredArray;
+    search(userInput){
+        var arr = this.state.UnfilteredArray;
+        var FilteredArray = [];
 
-        for (var i = 0; i < returnArr.length; i++){
-            if (returnArr[i].)
+        for (var i = 0; i < arr.length; i++){
+            if (arr[i].includes(this.state.userInput)) {
+                FilteredArray.push(arr[i])
+            }
         }
+
+        this.setState({FilteredArray: FilteredArray})
     }
 
     
@@ -28,10 +33,10 @@ export default class FilterStrings extends Component {
         return (
             <div className="puzzleBox filterStringPB">
                 <h4>Filter String</h4>
-                <span className="puzzleText">UnfilteredArray:</span>
+                <span className="puzzleText">UnfilteredArray: { JSON.stringify(this.state.UnfilteredArray, null, String) }</span>
                 <input className="inputLine" onChange={(e) => this.searchEntry(e.target.value)}></input>
-                <button className="confirmationButton">Filter</button>
-                <span className="resultsBox filterStringPB">FilteredArray:</span>
+                <button className="confirmationButton" onClick={() => {this.search(this.state.userInput)}}>Filter</button>
+                <span className="resultsBox filterStringPB">FilteredArray: { JSON.stringify(this.state.FilteredArray, null, 10) }</span>
             </div>
         )
     }
